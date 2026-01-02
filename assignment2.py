32637c85f8dece74d80783ee3afe2a8907649aab
ArdaBehramuni <arda.behram@std.yeditepe.edu.tr>
2026-01-02 21:52:36 +0300

    file loading and array crafting based on data

--- assignment2.py
+++ assignment2.py
@@ -15,16 +15,3 @@ def stat():
     new_data[:, 3][new_data[:, 3] < 40000] = 0
     
     return data, hare, min_year_hare, lynx_avg, new_data
-    
-    
-data = np.loadtxt("populations.txt", skiprows=1)
-    
-hare = data[:,1]
-
-min_year_hare = data[np.argmin(hare),0]
-
-lynx_avg = np.mean(data[:,2])
-
-new_data = np.hstack((data, (np.sum(data[:, 1:], axis=1)).reshape(21, 1)))
-
-new_data[:, 3][new_data[:, 3] < 40000] = 0
